cadd1d39876a75898068a26bb7571fe50c5c4c5d
HugoHerve <hugo.herve@isen-ouest.yncrea.fr>
2020-03-24 11:39:03 +0100

    modif

--- Cercle.py
+++ Cercle.py
@@ -3,24 +3,26 @@ class  Cercle:
         self.__rayon = rayon
 
     def __add__(self, other):
-        if isinstance(other, Cercle) :
+        if isinstance(other, Cercle):
             return Cercle(self.__rayon + other.__rayon)
         else: print("Les 2 objets doivent être de la même class")
     def __lt__(self, other):
-        if isinstance(other, Cercle) :
+        if isinstance(other, Cercle):
             return self.__rayon < other.__rayon
         else:print("Les 2 objets doivent être de la même class")
 
     def __gt__(self, other):
-        if isinstance(other, Cercle) :
+        if isinstance(other, Cercle):
             return self.__rayon > other.__rayon
         else:print("Les 2 objets doivent être de la même class")
 
 
-if __name__=='__Circle__':
+
+
+if __name__  == '__main__':
     c1 = Cercle(2)
     c2 = Cercle(3)
     c3 = c1 + c2
     c4 = c1 < c2
     c5 = c2 > c3
-    c5
+    c3
