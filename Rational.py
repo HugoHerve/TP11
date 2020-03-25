1930f7bc15fa513c8fbb5c778d08fdae96897472
HugoHerve <hugo.herve@isen-ouest.yncrea.fr>
2020-03-25 08:26:00 +0100

    Rational

---
+++ Rational.py
@@ -0,0 +1,38 @@
+class Rational:
+    def __init__(self, num, den):
+        self.__numerateur = num
+        self.__denominateur = den
+
+    def __add__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.__numerateur*other.__denominateur+other.__numerateur*self.__denominateur,self.__denominateur*other.__denominateur)
+        else: print("Les deux objets doivent être de la même class")
+
+    def __sub__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.__numerateur*other.__denominateur-other.__numerateur*self.__denominateur, self.__denominateur*other.__denominateur)
+        else: print("Les deux objets doivent être de la même classe")
+    def __mul__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.__numerateur*other.__numerateur, self.__denominateur*other.__denominateur)
+        else: print("Les deux objets doivent être de la même class")
+    def __truediv__(self, other):
+        if isinstance(other, Rational):
+            return (self.__numerateur*other.__denominateur, self.__denominateur*other.__numerateur)
+        else: print("Les deux objets doivent être de la même class")
+    def __str__(self):
+        return str(self.__numerateur)+"/"+str(self.__denominateur)
+
+if __name__  == '__main__':
+    f1 = Rational(1,2)
+    f2 = Rational(1,4)
+    f3 = f1+f2
+    f4 = f1-f2
+    f5 = f1*f2
+    f6 = f1/f2
+    print(f1)
+    print(f2)
+    print(f3)
+    print(f4)
+    print(f5)
+    print(f6)
